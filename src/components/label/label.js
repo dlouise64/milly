7bede4d777edc2e8f4ccc56caf49fe5ab7c4b146
dlouise64 <dana.iti.work@gmail.com>
2019-11-30 00:45:48 +1300

    label component

---
+++ src/components/label/label.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { LabelWrapper } from './styled-label'
+function Label({ children }) {
+	return <LabelWrapper>{children}</LabelWrapper>
+}
+
+export default Label
---
+++ src/components/label/styled-label.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LabelWrapper = styled.label`
+	display: block;
+	font-size: 0.75rem;
+	margin-bottom: 0.4375rem;
+	font-weight: 500;
+	letter-spacing: 0.0625rem;
+	text-transform: uppercase;
+`
