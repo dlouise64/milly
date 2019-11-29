a8506f74bc6107ef82620f4fd52c3be2369a363b
dlouise64 <dana.iti.work@gmail.com>
2019-11-30 00:45:56 +1300

    form component

---
+++ src/components/form/form.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { FormWrapper } from '../form/styled-form'
+
+function Form({ children }) {
+	return <FormWrapper>{children}</FormWrapper>
+}
+
+export default Form
---
+++ src/components/form/styled-form.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import style from '../variables'
+export const FormWrapper = styled.form`
+	max-width: 25rem;
+	margin: 1.25rem;
+	padding: 2.5rem;
+	box-shadow: 0 0 0 0.0625rem ${style.border.primary};
+	border-radius: 0.4375rem;
+`
